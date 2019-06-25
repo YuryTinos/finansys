d9c952335c38f12432461461b9031f07281b5f0e
yurytinos <yury.tinos@gmail.com>
2019-06-25 18:50:01 -0300

    Add Categories

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -4,14 +4,9 @@ import {CategoryListComponent} from './category-list/category-list.component';
 import {CategoryFormComponent} from './category-form/category-form.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: CategoryListComponent
-  },
-  {
-    path: ':id',
-    component: CategoryFormComponent
-  }
+  { path: '', component: CategoryListComponent },
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent },
 ];
 
 @NgModule({
