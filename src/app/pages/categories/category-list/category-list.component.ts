ef3493bb6cbf51366238f4163ff9d1a28b16e319
yurytinos <yury.tinos@gmail.com>
2019-07-13 12:23:00 -0300

    improving category list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {CategoryService} from '../shared/category.service';
+import {Category} from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) {
+  }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar Lista')
+    );
+  }
+
+  deleteCategory(category) {
+
+    const mustDelete = confirm('Deseja realmente deletar este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories
+          .filter(c => c.id !== category.id),
+        error => alert('Erro ao tentar Excluir')
+      );
+    }
   }
 
 }
