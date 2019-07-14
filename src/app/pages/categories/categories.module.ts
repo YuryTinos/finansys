3ac0e840c49f4186fd73392d6b3e618533570ed8
yurytinos <yury.tinos@gmail.com>
2019-07-14 11:43:38 -0300

    Project restructuring

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,17 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
-import {CategoriesRoutingModule} from './categories-routing.module';
-import {CategoryListComponent} from './category-list/category-list.component';
-import {CategoryFormComponent} from './category-form/category-form.component';
+import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
-    CommonModule,
-    CategoriesRoutingModule,
-    ReactiveFormsModule
+    SharedModule,
+    CategoriesRoutingModule
   ]
 })
 export class CategoriesModule {
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { EntriesRoutingModule } from './entries-routing.module';
-
-import { EntryListComponent } from './entry-list/entry-list.component';
-import { EntryFormComponent } from './entry-form/entry-form.component';
-
-import { CalendarModule } from 'primeng/primeng';
 import { IMaskModule } from 'angular-imask';
 
+import { CalendarModule } from 'primeng/primeng';
+
+import { SharedModule } from '../../shared/shared.module';
+
+import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
+
 @NgModule({
-  declarations: [ EntryListComponent, EntryFormComponent ],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
